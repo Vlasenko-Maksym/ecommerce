99141bb0e428bbfb64f10bbf925c8cf399741c05
Pablo Moricz <pmoricz@sicro.com.ar>
2019-05-18 19:58:54 -0300

    modificaciones barra menu

--- barramenu.php
+++ barramenu.php
@@ -1,59 +1,42 @@
 <nav>
-        <div class="container-fluid menu shadow-sm bg-white rounded">
-              <div class="row bg-light text-dark align-middle">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 logo">
-                          <a class="text-dark" href="#">BLACK<img class="logoimg" src="imagenes/blackfox.png" alt="logo">FOX</a>
-                    </div>
-                    <div class=" col-xs-6 col-sm-6 col-md-6  col-lg-2  marcas ">
-                          <a data-toggle="collapse" href="#collapseMarcas" role="button" aria-expanded="false" aria-controls="collapseMarcas">Marcas</a>
-                    </div>
-                    <div class="col-xs-6 col-sm-6  col-md-6  col-lg-2  registro">
-                          <a data-toggle="collapse" href="#collapseRegistro" role="button" aria-expanded="false" aria-controls="collapseRegistro">Loguin</a>
-                    </div>
-                    <div class="collapse mycollapse" id="collapseMarcas">
-                          <div class="card card-body mycard">
+      <div class="container-fluid menu shadow-sm bg-white rounded">
+            <div class="row bg-light text-dark align-middle accordion" id="padre">
+                  <!-- EN DISPOSITIVOS CHICOS-->
+                  <div class="col-sm-12 col-md-12 col-lg-8 logo">
+                        <a class="text-dark" href="#">BLACK<img class="logoimg" src="imagenes/blackfox.png" alt="logo">FOX</a>
 
-                                <a href="#"><img class="imgmarcas" src="imagenes/imgmarcas/cartier.png" alt="imgcartier"></a>
-                                <a href="#"><img class="imgmarcas" src="imagenes\imgmarcas\IWC_Schaffhausen_Logo.png" alt="imgIWCSchaffhausen"></a>
-                                <a href="#"><img class="imgmarcas" src="imagenes\imgmarcas\Jaeger-leCoultre.png" alt="imgJaeger"></a>
-                                <a href="#"><img class="imgmarcas" src="imagenes\imgmarcas\omega.png" alt="imgomega"></a>
-                                <a href="#"><img class="imgmarcas" src="imagenes\imgmarcas\Patek-Philippe.png" alt="imgPatek"></a>
-                                <a href="#"><img class="imgmarcas" src="imagenes/imgmarcas/rolex.png" alt="imgrolex"></a>
+                  </div>
+                  <div class="col-sm-6 col-md-6  col-lg-2 marcas ">
+                        <a  class="btnmenu"data-toggle="collapse" href="#collapseMarcas" role="button" aria-expanded="false" aria-controls="collapseMarcas">Galeria</a>
+                  </div>
 
-                          </div>
-                    </div>
-                  <!-- BOTON REGISTRO -->
-                    <div class="collapse mycollapse" id="collapseRegistro">
-                          <div class="card ">
-                                <div class="card-body">
-                                      <form class="form-signin">
-                                            <div class="form-label-group">
-                                                  <input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus>
-                                            </div>
+                  <div class="col-sm-6  col-md-6  col-lg-2  registro">
+                        <a class="btnmenu"data-toggle="collapse" href="#collapseRegistro" role="button" aria-expanded="false" aria-controls="collapseRegistro" >Login</a>
+                        <img src="imagenes\carro-compra3.png" alt="">
+                  </div>
+                  <div class="collapse mycollapse" id="collapseMarcas" data-parent="#padre">
+                        <div class="card card-body mycard">
+                              <a href="#"><img class="imgmarcas" src="imagenes\imgmarcas\omega.png" alt="imgomega"></a>
+                              <a href="#"><img class="imgmarcas" src="imagenes/imgmarcas/cartier.png" alt="imgcartier"></a>
+                              <a href="#"><img class="imgmarcas" src="imagenes\imgmarcas\Patek-Philippe.png" alt="imgPatek"></a>
+                              <a href="#"><img class="imgmarcas" src="imagenes\imgmarcas\IWC_Schaffhausen_Logo.png" alt="imgIWCSchaffhausen"></a>
+                              <a href="#"><img class="imgmarcas" src="imagenes/imgmarcas/rolex.png" alt="imgrolex"></a>
+                              <a href="#"><img class="imgmarcas" src="imagenes\imgmarcas\Jaeger-leCoultre.png" alt="imgJaeger"></a>
+                            <a href="#"> <div class="vertodas">Ver Todas +</div></a>
 
-                                            <div class="form-label-group">
-                                                  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
-                                            </div>
 
-                                            <hr>
 
-                                            <div class="form-label-group">
-                                                  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                                            </div>
+                        </div>
+                  </div>
+                <!-- BOTON REGISTRO -->
+                  <div class="collapse mycollapse" id="collapseRegistro" data-parent="#padre">
+                        <div class="card ">
+                              <div class="card-body">
+                                  <?php include "login.php";?>
+                              </div>
+                        </div>
+                  </div>
 
-                                            <div class="form-label-group">
-                                                  <input type="password" id="inputConfirmPassword" class="form-control" placeholder="Password" required>
-                                            </div>
-
-                                            <button class="btn btn-lg btn-danger btn-block text-uppercase" type="submit">Register</button>
-                                            <hr class="my-4">
-                                            <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign up with Google</button>
-                                            <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign up with Facebook</button>
-                                      </form>
-                                </div>
-                          </div>
-                    </div>
-
-              </div>
-        </div>
-  </nav>
+            </div>
+      </div>
+</nav>
---
+++ login.php
@@ -0,0 +1,49 @@
+﻿
+    <div class="container">
+		<div class="row login">
+			<div class="col-xl-6 col-md-6 col-sm-12 colRegistro">
+				<h2>Iniciar sesión</h2>
+				<div class="social-container">
+					<div class="mybutton">
+						<button class="fButton" type="button">f</button>
+						<button class="gButton"type="button">g</button>
+					</div>
+				</div>
+				<h6>o usa tu usuario</h6>
+				<form action="#">
+					<div class="fourm-group">
+						<input type="email" class="form-control" id="emailLogin" placeholder="Ingrese email">
+						<input type="password" class="form-control" id="passLogin" placeholder="Ingrese contraseña">
+						<a href="#">Olvidaste tu contraseña?</a>
+					</div>
+					<div class="row button-container">
+						<div class="col-12 ">
+								<button type="submit" class="btn btn-primary custom-btn">Logearse</button>
+						</div>
+  				</div>
+				</form>
+			</div>
+			<div class="col-xl-6 col-md-6 col-sm-12 colRegistro">
+				<h2>Registrarse</h2>
+				<div class="social-container">
+					<div class="mybutton">
+						<button class="fButton" type="button">f</button>
+						<button class="gButton"type="button">g</button>
+					</div>
+				</div>
+				<h6>o usa tu email para registrarte</h6>
+				<form action="#">
+					<div class="fourm-group register-container" >
+						<input type="text" class="form-control" id="nameRegister" placeholder="Ingrese nombre">
+						<input type="email" class="form-control" id="emailRegister" placeholder="Ingrese email">
+						<input type="password" class="form-control" id="passRegister" placeholder="Ingrese contraseña">
+					</div>
+					<div class="row button-container">
+						<div class="col-12">
+								<button type="submit" class="btn btn-primary custom-btn">Registrarse</button>
+						</div>
+  				</div>
+				</form>
+			</div>
+		</div>
+	</div>
